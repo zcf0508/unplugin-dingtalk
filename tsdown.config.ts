719c3ba16357a22c6c001046bb37abc3b3ac8959
zcf0508 <zcf0508@live.com>
2025-10-20 18:00:00 +0800

    chore: tsdown

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,11 +1,10 @@
-import type { Options } from 'tsup';
+import type { Options } from 'tsdown';
 
 export default <Options>{
-  entryPoints: [
+  entry: [
     'src/*.ts',
   ],
   clean: true,
   format: ['cjs', 'esm'],
   dts: true,
-  onSuccess: 'npm run build:fix',
 };
