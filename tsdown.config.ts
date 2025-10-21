a7acb5a1ffb292c543677076c2786457f2259634
zcf0508 <zcf0508@live.com>
2025-10-21 13:46:10 +0800

    fix: build error

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,10 +1,10 @@
 import type { Options } from 'tsdown';
 
-export default <Options>{
+export default {
   entry: [
     'src/*.ts',
   ],
   clean: true,
   format: ['cjs', 'esm'],
   dts: true,
-};
+} satisfies Options;
